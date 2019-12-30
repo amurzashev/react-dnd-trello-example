5d500d4de2e2ea081fd02564334278453d71909c
Abylay Murzashev <abylay.murzashev@gmail.com>
2019-12-30 23:51:11 +0600

    bg of body

--- src/components/molecules/List/index.js
+++ src/components/molecules/List/index.js
@@ -4,10 +4,8 @@ export default styled.div`
   & > div:not(:first-of-type) {
     margin-top: 10px;
   }
-  border: 1px solid black;
   padding: 8px;
   width: 320px;
-  border-radius: 3px;
   &:not(:first-of-type) {
     margin-left: 10px;
   }
