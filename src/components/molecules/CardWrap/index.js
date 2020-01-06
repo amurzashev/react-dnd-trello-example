7d9ed78452c5dd284c74d7491be944455c0e03bb
Abylay Murzashev <abylay.murzashev@gmail.com>
2020-01-07 00:05:39 +0600

    its ok for now

--- src/components/molecules/CardWrap/index.js
+++ src/components/molecules/CardWrap/index.js
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 
 // unsafe must find another way
 export default styled.div`
-padding: 0 0 60px 0;
-  & > *:not(:first-child) {
+  padding: 0 0 60px 0;
+  & > *:not(:first-of-type) {
     margin-top: 10px;
   }
 `;
