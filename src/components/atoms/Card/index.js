619db6fe598c44e2910496b1dae0c553e0b56be4
Abylay Murzashev <abylay.murzashev@gmail.com>
2020-01-07 14:33:30 +0600

    minor css

--- src/components/atoms/Card/index.js
+++ src/components/atoms/Card/index.js
@@ -4,6 +4,7 @@ export default styled.div`
   border-radius: 3px;
   padding: 8px;
   background: ${props => props.isDragging ? props.theme.colors.cardBgHover : props.theme.colors.cardBg};
+  opacity: ${props => props.isDragging ? 0.7 : 1};
   color: ${props => props.theme.colors.text};
   cursor: pointer;
   min-height: 37px;
--- src/components/molecules/CardWrap/index.js
+++ src/components/molecules/CardWrap/index.js
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 // unsafe must find another way
 export default styled.div`
-  padding: 0 0 60px 0;
+  min-height: 120px;
   & > *:not(:first-of-type) {
     margin-top: 10px;
   }
